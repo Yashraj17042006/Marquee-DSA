052c9140b99c89abdc56335aaae0c5b8c99647b4
Yashraj17042006 <yashrajmaharaul@gmail.com>
2026-05-20 09:10:18 +0530

    Printing Pattern Question

--- DSA/pattern1.java
+++ DSA/pattern1.java
@@ -1,11 +1,12 @@
-import java.util.*;
-public class pattern {
+import java.util.Scanner;
+
+public class pattern1 {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         for(int i=0;i<=n;i++){
-            for(int j=0;j<=n;j++){
-                System.out.print(i+""+j+" ");
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
             }
             System.out.println();
         }
--- DSA/pattern3.java
+++ DSA/pattern3.java
@@ -1,11 +1,11 @@
-import java.util.*;
-public class pattern {
+import java.util.Scanner;
+public class pattern3{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=n;j++){
-                System.out.print(i+""+j+" ");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*"+" ");
             }
             System.out.println();
         }

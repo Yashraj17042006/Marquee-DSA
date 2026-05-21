9999a88d6f085d83da9c25ddd990157b653f878f
Yashraj17042006 <yashrajmaharaul@gmail.com>
2026-05-21 09:43:58 +0530

    Printing Pattern Question

---
+++ DSA/pattern11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class pattern11 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i =n; i>=1; i--) {
+            for (int j =i ;j<=n-1; j++) {
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+

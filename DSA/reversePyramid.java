c3ff0ba5e74a816e6dfdbe264e6716f8ab9c9d9e
Yashraj17042006 <yashrajmaharaul@gmail.com>
2026-05-23 07:42:50 +0530

    Printing Pattern Question

--- DSA/reversePyramid.java
+++ DSA/reversePyramid.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-public class pattern10 {
+public class reversePyramid {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        for (int i = 1; i<=n; i++) {
+        for (int i =n; i>=1; i--) {
             for (int j =i ;j<=n-1; j++) {
                 System.out.print(" ");
             }
